a74b5abd0e3f4df3c36901020b17cb88f94795af
Rodrigo <rodrigopba1@gmail.com>
2020-11-30 17:53:47 -0300

    add encrp and decrp

---
+++ concepts and implementations/libs.py
@@ -0,0 +1,7 @@
+#Signatures.py
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives import serialization
