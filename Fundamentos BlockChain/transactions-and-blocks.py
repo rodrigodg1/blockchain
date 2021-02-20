b4b66665c33635d8c0fa4cb801b4cd161559d65d
Rodrigo <73913021+rodrigodg1@users.noreply.github.com>
2021-02-20 15:29:01 -0400

    Update transactions-and-blocks.py

--- Fundamentos BlockChain/transactions-and-blocks.py
+++ Fundamentos BlockChain/transactions-and-blocks.py
@@ -5,43 +5,50 @@ Created on Sat Feb 20 09:37:13 2021
 
 @author: rodrigodutra
 """
-
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+import pickle
 
 
+#gera a chave privada e publica
 def generate_keys():
-        private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
-        )
-        # gera a chave pública
-        public_key = private_key.public_key()
+    private = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )    
+    public = private.public_key()
+    pu_ser = public.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )    
+    return private, pu_ser
 
-        return private_key, public_key
+#assina a mensagem com sua chave privada
+def sign(message, private):
+    message = bytes(str(message), 'utf-8')
+    sig = private.sign(
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return sig
 
-
-
-# assina a transação/mensagem com a chave privada
-def sign(message, private_key):
-     message = bytes(str(message), 'utf-8')
-     signature = private_key.sign(
-     message,
-     padding.PSS(
-         mgf=padding.MGF1(hashes.SHA256()),
-         salt_length=padding.PSS.MAX_LENGTH
-     ),
-     hashes.SHA256()
-     )
-     return signature
-
-
-
-# verifica a validade a partir da chave publica e assinatura
-def verify(message, sig, public):
+#verifica a validade da mensagem, com a assinatura e chave publica
+def verify(message, sig, pu_ser):
+    public = serialization.load_pem_public_key(
+        pu_ser,
+        backend=default_backend()
+    )
+    
     message = bytes(str(message), 'utf-8')
     try:
         public.verify(
@@ -59,8 +66,6 @@ def verify(message, sig, public):
     except:
         print("Error executing public_key.verify")
         return False
-
-
 
 
 
@@ -143,6 +148,23 @@ class Tx:
         data.append(self.reqd)
         return data
     
+    
+    
+    def __repr__(self):
+        reprstr = "INPUTS:\n"
+        for addr, amt in self.inputs:
+            reprstr = reprstr + str(amt) + " from " + str(addr) + "\n"
+        reprstr = reprstr + "OUTPUTS:\n"
+        for addr, amt in self.outputs:
+            reprstr = reprstr + str(amt) + " to " + str(addr) + "\n"
+        reprstr = reprstr + "REQD:\n"
+        for r in self.reqd:
+            reprstr = reprstr + str(r) + "\n"
+        reprstr = reprstr + "SIGS:\n"
+        for s in self.sigs:
+            reprstr = reprstr + str(s) + "\n"
+        reprstr = reprstr + "END\n"
+        return reprstr
   
     
     def sign(self, private):
@@ -231,46 +253,157 @@ class Block:
 
 
     
+
+
+
+
+
+
+class TxBlock (Block):
+
+    valid_transactions_count = 0
     
-#criação das contas
-acc1 = Account(10)
-acc2 = Account(0)
+
+    def __init__(self, previousBlock):
+        super(TxBlock, self).__init__([],previousBlock)
+        
+
+# adiciona uma transação ao bloco como dados
+#contagem das transações válidas no bloco
+    def count_valid_transactions(self):
+        for tx in self.data:
+            if tx.is_valid():
+                self.valid_transactions_count = self.valid_transactions_count + 1
+            
+        return self.valid_transactions_count
+                
 
 
-#print(acc1.get_wallet())
-#print(acc2.get_wallet())
+    def addTx(self, Tx_in):
+        self.data.append(Tx_in)
 
 
-#cria uma transação da conta 1 transferindo para a conta 2
-tx1 = Tx()
-tx1.add_input(acc1.get_public_key(),acc1.get_amount())
-tx1.add_output(acc2.get_public_key(),2)
-tx1.sign(acc1.get_private_key())
+#verifica se as transações no bloco são válidas
+#percorre as transações armazenadas como dados no bloco
+#e verifica se essas transações são válidas
+    def is_valid(self):
+        if not super(TxBlock, self).is_valid():
+            return False
+        for tx in self.data:
+            if not tx.is_valid():
+                return False
 
-tx2 = Tx()
-tx2.add_input(acc1.get_public_key(),acc1.get_amount())
-tx2.add_output(acc2.get_public_key(),2)
-tx2.sign(acc1.get_private_key())
-
-
-      
-if(tx1.is_valid()):
-    print("Transação Válida")
-   
-else:
-    print("Transação Inválida")
+        return True
 
 
 
 
+class Blockchain:
+    count_blocks = 0
+    chain = []
+    id_chain = ""
 
-#adiciona nos blocos, as transações
-#root nao tem bloco anterior, por isso none
-root = Block(tx1,None)
+    #nome para a cadeia
+    def __init__(self,id_chain):
+        self.id_chain = id_chain
 
-block1 = Block(tx2,root)
+    def add_block(self,block):
+        self.chain.append(block)
+        self.count_blocks = self.count_blocks + 1
 
-#mostra os blocos
-for i in [root,block1]:
-    print (i.id_block)
+
+
+
+
+if __name__ == '__main__':
+
+    #criação das contas
+    acc1 = Account(10)
+    acc2 = Account(0)
+    
+    
+    #print(acc1.get_wallet())
+    #print(acc2.get_wallet())
+    
+    
+    #cria uma transação da conta 1 transferindo para a conta 2
+    tx1 = Tx()
+    tx1.add_input(acc1.get_public_key(),acc1.get_amount())
+    tx1.add_output(acc2.get_public_key(),2)
+    tx1.sign(acc1.get_private_key())
+    
+    tx2 = Tx()
+    tx2.add_input(acc1.get_public_key(),acc1.get_amount())
+    tx2.add_output(acc2.get_public_key(),2)
+    tx2.sign(acc1.get_private_key())
+    
+    
+    
+    tx3 = Tx()
+    tx3.add_input(acc2.get_public_key(),acc2.get_amount())
+    tx3.add_output(acc1.get_public_key(),4)
+    tx3.sign(acc2.get_private_key())
+    
+    
+    
+    
+    
+          
+    if(tx1.is_valid()):
+        print("Transação Válida")
+       
+    else:
+        print("Transação Inválida")
+    
+    
+    
+    #adiciona nos blocos, as transações
+    #root nao tem bloco anterior, por isso none
+    root = TxBlock(None)
+    root.addTx(tx1)
+    root.addTx(tx2)
+    
+    
+    block1 = TxBlock(root)
+    block1.addTx(tx2)
+    
+    
+    #salva as transações em um arquivo
+    savefile = open("block.dat", "wb")
+    pickle.dump(block1, savefile)
+    savefile.close()
+    
+ 
+    #carrega esse arquivo
+    loadfile = open("block.dat", "rb")
+    load_B1 = pickle.load(loadfile)
+    
+    
+    #verifica se esse arquivo é valido
+    if load_B1.is_valid():
+        print("Sucess! Loaded Tx is valid")
+    else:
+        print("Fail ! Tx block is invalid")
+    loadfile.close()
+    
+    
+    
+    #cria um blockchain
+    blockchain = Blockchain("My-Chain")
+    
+    print("\n\n"+ blockchain.id_chain)
+
+    
+    # verifica os blocos criados aqui e tbm dos arquivos
+    for b in [root, block1,load_B1]:
+        print(f"\nblock {b.id_block}")
+        if b.is_valid():
+            print("Success! Valid block")
+            print("Inserting into blockchain...")
+            blockchain.add_block(b)
+        else:
+            print("ERROR! Bad block")
+
+    # mostra a quantidade de blocos na cadeia
+    print(f"\n\nCount block (height) blockchain: {blockchain.count_blocks}")
 
