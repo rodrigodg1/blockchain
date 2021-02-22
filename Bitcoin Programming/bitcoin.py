018fd67fbbd7238aba6a248b794d6dd35887e961
Rodrigo <73913021+rodrigodg1@users.noreply.github.com>
2021-02-21 21:58:22 -0400

    Update bitcoin.py

--- Bitcoin Programming/bitcoin.py
+++ Bitcoin Programming/bitcoin.py
@@ -1,1 +1,17 @@
-#!/usr/bin/env python3# -*- coding: utf-8 -*-"""Created on Sun Feb 21 08:28:58 2021@author: rodrigodutra"""from bitcoin import *#gerando chave privadamy_private_key = random_key()#gerando chave publica (é feito a partir da chave privada)my_public_key = privtopub(my_private_key)print(f"Private Key: {my_private_key}")print(f"Public Key: {my_public_key}")# e com a chave pública, geramos o endereço (Address)my_bitcoin_address = pubtoaddr(my_public_key)print(f"My bitcoin address: {my_bitcoin_address} ")
+
+from bitcoin import *
+
+#gerando chave privada
+my_private_key = random_key()
+
+#gerando chave publica (é feito a partir da chave privada)
+my_public_key = privtopub(my_private_key)
+
+print(f"Private Key: {my_private_key}")
+print(f"Public Key: {my_public_key}")
+
+
+
+# e com a chave pública, geramos o endereço (Address)
+my_bitcoin_address = pubtoaddr(my_public_key)
+print(f"My bitcoin address: {my_bitcoin_address} ")
